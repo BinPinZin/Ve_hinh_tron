60757628975515a87149568627da6f1376a182be
unknown <sonthoren@gmail.com>
2021-08-12 17:43:47 +0700

    Vehinhtron1

---
+++ ve_hinh_tron.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.speed(10)
+turtle.pensize (5)
+
+turtle.pencolor ("red")
+
+
+turtle.fillcolor ("red")
+turtle.begin_fill()
+
+
+turtle.circle (150)
+turtle.end_fill()
+
+turtle.done()
